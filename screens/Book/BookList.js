ff6e189319bfd7d144b90e5086886b5efcb6a27d
kchaou-fakhri <kchaoufakhri@gmail.com>
2023-02-14 16:39:59 +0100

    feat() : Create Book List Screen - create BookList screen. feat : #feature/list_books

---
+++ screens/Book/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, SafeAreaView} from "react-native";
+
+
+export default function BookList() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hello to Book List</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
